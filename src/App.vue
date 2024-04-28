a27e0be02955a525fd2d4af9ad590fbca11b7abc
Michael Kurz <michael_kurz@posteo.at>
2024-04-28 16:50:43 +0200

    add login links

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div>Hello {{ value }}</div>
+  <a href="/.auth/login/github?post_login_redirect_uri=https://webhook.site/5cdf1105-34cf-4af3-9c64-04d16ebcf921">Login via GitHub</a>
+  <a href="/.auth/login/aad?post_login_redirect_uri=https://webhook.site/5cdf1105-34cf-4af3-9c64-04d16ebcf921">Login via Microsoft Entra ID</a>
 </template>
 
 <script>
