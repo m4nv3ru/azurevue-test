7419a4b618a6c2d3ed3b3d7d688ec369357790f0
Michael Kurz <michael_kurz@posteo.at>
2024-05-05 16:14:02 +0200

    update api functions

--- api/src/functions/httpTrigger1.js
+++ api/src/functions/httpTrigger1.js
@@ -4,7 +4,15 @@ app.http('httpTrigger1', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        const result = await (await fetch("./auth/me")).json();
-        return { body: result };
+        try {
+            const result = await (await fetch("./auth/me")).json();
+            return { body: JSON.stringify(result) };
+        } catch (error) {
+            return {
+                status: 500,
+                body: { message: error.message }
+            };
+        }
+        
     }
 });
